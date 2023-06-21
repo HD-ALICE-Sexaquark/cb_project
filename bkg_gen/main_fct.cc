b335c1955e368f9b804abf676db55b38c3bba34c
Andrés Bórquez <andres.borquezc@gmail.com>
2023-06-21 17:20:21 +0200

    Bkg. gen.: now prints mother info - no longer print daughter info for final particles - added more comments - if theta selection doesn't have particles, retry

--- bkg_gen/main_fct.cc
+++ bkg_gen/main_fct.cc
@@ -29,11 +29,19 @@ int main(int argc, char* argv[]) {
     // prepare output file
     std::ofstream output_file;
 
-    // define to use later
+    // string and auxiliary types to help with formatting
     std::string output_filename;
     std::stringstream aux_ss;
-    char buffer_a[24];
+    char buffer_a[48];
     char buffer_b[96];
+
+    // aux. variables to hold mother's pid
+    int mother1;
+    int mother2;
+    int id_mother;
+
+    // counter of valid particles
+    int n_particles;
 
     // event loop
     for (int iEvent = 0; iEvent < nEvent; iEvent++) {
@@ -45,14 +53,21 @@ int main(int argc, char* argv[]) {
 
         // prepare output filename
         if (nEvent == 1) {
+            // test value
             output_filename = "../reco/bkg.csv";
         } else {
             aux_ss.clear();
-            sprintf(buffer_a, "event%03d_bkg.csv", iEvent);
+            sprintf(buffer_a, "event%03d_bkg.csv", iEvent);  // format string, insert int as a 3-digit number
             aux_ss << buffer_a;
             aux_ss >> output_filename;
         }
         output_file.open(output_filename);
+
+        // reset counter
+        n_particles = 0;
+
+        // (debug)
+        std::cout << "main_fct :: >> Event " << iEvent << std::endl;
 
         // particle loop -- print particle info
         for (int i = 0; i < pythia.event.size(); i++) {
@@ -67,19 +82,42 @@ int main(int argc, char* argv[]) {
                 continue;
             }
 
-            sprintf(buffer_b, "%i,%i,%i,%i,%.8e,%.8e,%.8e\n",                                                                  //
-                    pythia.event[i].status(), pythia.event[i].id(), pythia.event[i].daughter1(), pythia.event[i].daughter2(),  //
+            // get mother indices
+            mother1 = pythia.event[i].mother1();
+            mother2 = pythia.event[i].mother2();
+            // assign mother's pid
+            // verify the "normal" mother case, where it is meaningful to speak of one single mother to several products
+            // otherwise, set PID of mother to 0
+            id_mother = (mother1 > 0 && mother2 == 0) ? pythia.event[mother1].id() : 0;
+
+            sprintf(buffer_b, "%i,%i,%i,%.8e,%.8e,%.8e\n",                      //
+                    pythia.event[i].status(), pythia.event[i].id(), id_mother,  //
                     pythia.event[i].px(), pythia.event[i].py(), pythia.event[i].pz());
 
             // (debug)
-            std::cout << buffer_b;
+            std::cout << "main_fct :: " << buffer_b;
 
             // (output)
             output_file << buffer_b;
+
+            // update counter
+            n_particles++;
         }
 
         output_file.close();
-        std::cout << output_filename << " has been generated" << std::endl;
+
+        if (n_particles) {
+            // (debug)
+            std::cout << "main_fct :: >> " << n_particles << " particles have been generated and stored in " << output_filename
+                      << std::endl;
+        } else {
+            // (debug)
+            std::cout << "main_fct :: >> " << n_particles << " particles have been generated. Running it again..." << std::endl;
+            iEvent--;
+        }
+
+        // (debug) empty line
+        std::cout << std::endl;
 
     }  // end event loop
 
